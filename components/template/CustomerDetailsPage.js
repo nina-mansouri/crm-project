9a0ed49c9843198ae370a9a1f2e26001ef44eecc
Nina <n.mansouri2486@gmail.com>
2025-08-07 21:32:47 +0330

    Update CustomerDetailsPage.js

--- components/template/CustomerDetailsPage.js
+++ components/template/CustomerDetailsPage.js
@@ -5,7 +5,7 @@ import React from "react";
 function CustomerDetailsPage({ data }) {
     const router = useRouter()
     const deleteHandler = async() => {
-        const res = await fetch(`${process.env.BASE_URL}/api/delete/${data._id}`,{
+        const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/delete/${data._id}`,{
             method: "DELETE",
         })
         const newRes = await res.json()
